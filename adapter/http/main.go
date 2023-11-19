65cca0732bfb1d4861b151b909358ee08a5d2e76
airton.lirajunior <airton.lirajunior@bemobi.com>
2023-11-19 12:03:28 -0300

    adicionado prometheus e promhttp.handler

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/AirtonLira/system_planning_financial.git/adapter/http/infra"
 	"github.com/AirtonLira/system_planning_financial.git/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // Init - function initial application
@@ -13,6 +14,7 @@ func Init() {
 	http.HandleFunc("/", transaction.GetTransaction)
 	http.HandleFunc("/create", transaction.CreateTransaction)
 	http.HandleFunc("/health", infra.Health)
+	http.Handle("/metrics", promhttp.Handler())
 
 	fmt.Println("Listening 8090.......")
 	http.ListenAndServe(":8090", nil)
